fb419e6b443d4cdbe8114cf32fcdcbab94dc08b8
Jasjeet393Singh <81762166+Jasjeet393Singh@users.noreply.github.com>
2021-06-19 20:35:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,27 @@ function take_snapshot() {
 console.log('ml5 version:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/f8ad5bOUO/',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+
+function gotResult(error, results) {
+
+  if (error) {
+    console.error(error);
+  } else {
+
+    console.log(results);
+    document.getElementById("result_family_member_name").innerHTML = results[0].label;
+    document.getElementById("result_family_member_accuracy").innerHTML = results[0].confidence.toFixed(9);
+  }
+}
